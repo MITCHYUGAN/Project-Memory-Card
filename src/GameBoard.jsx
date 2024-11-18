5794c4e70c1cfed16b757b223d3bcc511e77e10b
MITCHYUGAN <denmichigan123@gmail.com>
2024-11-18 10:52:12 +0100

    feat: Wrote the Calculate score function to increase the score when a valid(not clicked before) Item is click

--- src/GameBoard.jsx
+++ src/GameBoard.jsx
@@ -1,11 +1,28 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react"
 import gameData from "./GameData"
 
 const GameBoard = ({ setScore, score, bestScore, setBestScore }) => {
+
+    const [clickedKeys, setClickedKeys] = useState([])
+
+    function calScore(id) {
+        if (clickedKeys.includes(id)) {
+
+            // Reset the current score
+            setScore(0)
+        } else {
+            // Add the new ID to the clickedKeys array
+            setClickedKeys([...clickedKeys, id])
+            // Increment the score
+            setScore(score + 1)
+        }
+    }
+
     return (
         <section className="gameboard">
             {gameData.map(({ id, img, name }) => (
-                <div className="game" key={id}>
+                <div className="game" key={id} onClick={() => calScore(id)}>
                     <img src={img} alt="" />
                     <p>{name}</p>
                 </div>
