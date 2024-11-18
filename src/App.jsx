61f8c16d65886192b08ee10960112a247af4963e
MITCHYUGAN <denmichigan123@gmail.com>
2024-11-18 10:59:52 +0100

    feat: Added the score and bestScore useState hooks and pass them as props to individual component for use, so the score and bestscore can update across the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { useState } from 'react'
 import './App.css'
+import GameBoard from './GameBoard'
+import ScoreBoard from './ScoreBoard'
 
 function App() {
 
+  const [score, setScore] = useState(0)
+  const [bestScore, setBestScore] = useState(0)
+
   return (
-    <main>
+    <main className='app'>
       <h1>Memory Game</h1>
+      <ScoreBoard score={score} bestScore={bestScore}/>
+      <GameBoard score={score} setScore={setScore} bestScore={bestScore} setBestScore={setBestScore}/>
     </main>
   )
 }
